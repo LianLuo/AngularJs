ff7f80bd654538b36fdbb9db208312d59f729d14
luoian <392193761@qq.com>
2020-09-16 23:59:46 +0800

    add employees component

--- AngularForDotnet/src/app/app.module.ts
+++ AngularForDotnet/src/app/app.module.ts
@@ -2,20 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 import { BankAccountComponent } from './bank-account/bank-account.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BankAccountComponent
+    BankAccountComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule, 
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularForDotnet/src/app/services/employee.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Employee } from "../models/employee.module";
+import { environment } from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService  {
+
+  formData:Employee;
+  list:Employee[];
+  private readonly employeeUrl = `${environment.apiBaseUrl}employee`;
+  private headerOption = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  getEmployeeById(employeeId:number){
+    return this.http.get(this.employeeUrl+`/${employeeId}`);
+  }
+
+  getEmployees(){
+    return this.http.get(this.employeeUrl).toPromise().then(res=>{
+      console.info(res);
+      this.list = res as Employee[];
+    });
+  }
+
+  createEmployee(emp:Employee){
+    console.info(emp,this.employeeUrl);
+    return this.http.post(this.employeeUrl,emp, this.headerOption);
+  }
+
+  updateEmployee(employee:Employee){
+    return this.http.put(this.employeeUrl+`/${employee.EmployeeID}`, employee);
+  }
+
+  deleteEmployee(employee:Employee|number){
+    const id = typeof(employee) == 'number'?employee:employee.EmployeeID;
+    return this.http.delete(this.employeeUrl+`/${id}`);
+  }
+}
