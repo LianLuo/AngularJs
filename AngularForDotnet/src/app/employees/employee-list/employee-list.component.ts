3ed83d19493c974cf34c1dff9b3c9d58015643e2
luoian <392193761@qq.com>
2020-09-18 00:08:01 +0800

    completed bankAccount and Employee register.

--- AngularForDotnet/src/app/employees/employee-list/employee-list.component.ts
+++ AngularForDotnet/src/app/employees/employee-list/employee-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Employee } from 'src/app/models/employee.model';
 import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
@@ -9,11 +11,24 @@ import { EmployeeService } from 'src/app/services/employee.service';
 export class EmployeeListComponent implements OnInit {
 
   constructor(
-    public service:EmployeeService
+    public service:EmployeeService,
+    private toastr:ToastrService
   ) { }
 
   ngOnInit(): void {
     this.service.getEmployees();
   }
 
+  onSelected(emp:Employee){
+    this.service.formData = Object.assign({},emp);
+  }
+  onDelete(emp:Employee|number){
+    if(confirm("Are you sure to delete this recored?"))
+    {
+      this.service.deleteEmployee(emp).subscribe(res=>{
+        this.toastr.success("Deleted Successful.","Employee Register.");
+        this.service.getEmployees();
+      });
+    }
+  }
 }
--- AngularForDotnet/src/app/services/bank-account.service.ts
+++ AngularForDotnet/src/app/services/bank-account.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { BankAccount } from "../models/bank-account.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BankAccountService {
 
-  constructor() { }
+  private bankAccountUrl = `${environment.apiBaseUrl}bankaccount`;
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  createBankAccount(bankAccount:BankAccount){
+    return this.http.post(this.bankAccountUrl,bankAccount);
+  }
+
+  updateBankAccount(bankAccount:BankAccount){
+    return this.http.put(this.bankAccountUrl+'/'+bankAccount.BankAccountID,bankAccount);
+  }
+
+  deleteBankAccount(bankAccount:BankAccount|number){
+    const id = typeof(bankAccount) == "number"?bankAccount:bankAccount.BankAccountID;
+    return this.http.delete(this.bankAccountUrl+"/"+id);
+  }
+
+  getBankAccount(id:number){
+    return this.http.get(this.bankAccountUrl+"/"+id);
+  }
+
+  fetchBankAccounts(){
+    return this.http.get(this.bankAccountUrl);
+  }
 }
