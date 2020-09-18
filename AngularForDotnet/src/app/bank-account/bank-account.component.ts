1a777144d20cd9db996b7bd009fe11d718a46a06
luoian <392193761@qq.com>
2020-09-18 23:43:59 +0800

    change BankAccount.

--- AngularForDotnet/src/app/bank-account/bank-account.component.ts
+++ AngularForDotnet/src/app/bank-account/bank-account.component.ts
@@ -3,6 +3,7 @@ import { FormArray, FormBuilder, FormGroup, NgForm, Validators } from '@angular/
 import { BankService } from "../services/bank.service";
 import { BankAccountService } from "../services/bank-account.service";
 import { BankAccount } from '../models/bank-account.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-bank-account',
@@ -11,67 +12,81 @@ import { BankAccount } from '../models/bank-account.model';
 })
 export class BankAccountComponent implements OnInit {
 
-  bankAccountForms:FormArray = this.fb.array([]);
-  bankList=[];
+  bankAccountForms: FormArray = this.fb.array([]);
+  bankList = [];
 
   constructor(
-    private fb:FormBuilder,
-    private bankService:BankService,
-    private service:BankAccountService
-    ) { }
+    private fb: FormBuilder,
+    private bankService: BankService,
+    private service: BankAccountService,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
     this.refreshList();
-    this.bankService.getBankList().subscribe(res=>{
+    this.bankService.getBankList().subscribe(res => {
       this.bankList = res as [];
     });
   }
 
-  addBackAccountForm(form?:NgForm){
+  addBackAccountForm(form?: NgForm) {
     this.bankAccountForms.push(this.fb.group({
-      BankAccountID:[0],
-      AccountNumber:['', Validators.required],
-      AccountHolder:['',Validators.required],
-      BankID:[0,Validators.min(1)],
-      Remark:['',Validators.required]
+      BankAccountID: [0],
+      AccountNumber: ['', Validators.required],
+      AccountHolder: ['', Validators.required],
+      BankID: [0, Validators.min(1)],
+      Remark: ['', Validators.required]
     }));
-    
+
   }
 
-  refreshList(){
-    this.service.fetchBankAccounts().subscribe(result=>{
-      (result as BankAccount[]).forEach(f=>{
-        this.bankAccountForms.push(this.fb.group({
-          BankAccountID:[f.BankAccountID],
-          AccountNumber:[f.AccountNumber, Validators.required],
-          AccountHolder:[f.AccountHolder,Validators.required],
-          BankID:[f.BankID,Validators.min(1)],
-          Remark:[f.Remark,Validators.required]
-        }));
-      });
+  refreshList() {
+    this.service.fetchBankAccounts().subscribe(result => {
+      if(result == [])
+      {
+        this.addBackAccountForm();
+      }else{
+        this.bankAccountForms.clear();
+        (result as BankAccount[]).forEach(f => {
+          this.bankAccountForms.push(this.fb.group({
+            BankAccountID: [f.BankAccountID],
+            AccountNumber: [f.AccountNumber, Validators.required],
+            AccountHolder: [f.AccountHolder, Validators.required],
+            BankID: [f.BankID, Validators.min(1)],
+            Remark: [f.Remark, Validators.required]
+          }));
+        });
+      }
+      
     });
   }
 
-  onSubmit(fg:FormGroup){
+  onSubmit(fg: FormGroup) {
     const bankAccount = fg.value as BankAccount;
-    if(bankAccount != null && bankAccount.BankAccountID != 0)
-    {
-      this.service.updateBankAccount(bankAccount).subscribe(res=>{
+    if (bankAccount != null && bankAccount.BankAccountID != 0) {
+      this.service.updateBankAccount(bankAccount).subscribe(res => {
+        this.toastr.info("Updated Successfual.", "Bank Account.");
         this.refreshList();
       });
     }
-    else{
-      this.service.createBankAccount(bankAccount).subscribe((f:any)=>{
-        fg.patchValue({BankAccountID:f.BankAccountID});
+    else {
+      this.service.createBankAccount(bankAccount).subscribe((f: any) => {
+        fg.patchValue({ BankAccountID: f.BankAccountID });
+        this.toastr.info("Inserted Successfual.", "Bank Account.");
       });
     }
-    
+
   }
 
-  onDelete(fg:FormGroup,i:number){
-    this.service.deleteBankAccount(fg.value).subscribe(res=>{
+  onDelete(bankAccountID:number, i: number) {
+    if (bankAccountID == 0) {
       this.bankAccountForms.removeAt(i);
-    });
+    } else if (confirm("Are you sure to delete this record?")) {
+      this.service.deleteBankAccount(bankAccountID).subscribe(res => {
+        this.bankAccountForms.removeAt(i);
+        this.toastr.warning("Deleted Successfual.", "Bank Account.");
+      });
+    }
   }
 
 }
